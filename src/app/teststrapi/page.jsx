46595d1d45e5d1800b66a2b5a142128fbc184c25
shazebkhan22 <shazeb.khan@somaiya.edu>
2025-10-03 15:45:19 +0530

    Commented strapi code

--- src/app/teststrapi/page.jsx
+++ src/app/teststrapi/page.jsx
@@ -1,25 +1,25 @@
 
-async function loader(){
-    const path = "/api/home-page";
-    const BASE_URL = "http://localhost:1337";
-    const url = new URL(path, BASE_URL);
+// async function loader(){
+//     const path = "/api/home-page";
+//     const BASE_URL = "http://localhost:1337";
+//     const url = new URL(path, BASE_URL);
 
-    const response = await fetch(url);
-    const data = await response.json();
+//     const response = await fetch(url);
+//     const data = await response.json();
 
-    console.log(data);
+//     console.log(data);
 
-    return {...data.data}
-}
+//     return {...data.data}
+// }
 
-export default async function TestPage(){
-    const data = await loader();
-    console.log(data);
+// export default async function TestPage(){
+//     const data = await loader();
+//     console.log(data);
 
-    return (
-        <div>
-            {data.title} <br/>
-            {data.description}
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             {data.title} <br/>
+//             {data.description}
+//         </div>
+//     )
+// }
